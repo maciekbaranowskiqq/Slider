6fa329c3ec76bf5500d606f4e965d192edc23123
Maciej Baranowski <maciek.baranowski.qq@gmail.com>
2015-07-07 16:24:59 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TheWindow w = new TheWindow();
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		w.setSize(280,300);
+		w.setVisible(true);
+	}
+
+}
